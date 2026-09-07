3e91a3ae238e02f60229f218e2414b984656afbc
dkgsanchez <dkg.sanchez@gmail.com>
2026-09-07 11:21:35 +1000

    refactor: separate database setup

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,17 +1,9 @@
 import express from 'express';
-import pg from 'pg';
+import { pool } from './db.js';
 
-const { Pool } = pg;
 const app = express();
 app.use(express.json());
 const PORT = 3000;
-const pool = new Pool({
-    user: process.env.DB_USER,
-    host: process.env.DB_HOST,
-    database: process.env.DB_NAME,
-    password: process.env.DB_PASSWORD,
-    port: Number(process.env.DB_PORT)
-});
 
 type Workout = {
     id: number;
