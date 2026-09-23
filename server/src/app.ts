ed6205432c0a458410f37c54cae2b5166b40ed68
dkgsanchez <dkg.sanchez@gmail.com>
2026-09-23 22:38:24 +1000

    chore: configure CORS for frontend

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,8 +1,12 @@
 import express from 'express';
+import cors from 'cors';
 import { router as workoutRouter } from './routes/workoutRoutes.js';
 
 const app = express();
 app.use(express.json());
+app.use(cors({
+    origin: 'http://localhost:5173'
+}));
 app.use('/workouts', workoutRouter);
 
 app.get('/', (req, res) => {
